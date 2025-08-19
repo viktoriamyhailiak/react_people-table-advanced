02551ac3a45ac40222986212b3df51f43ad0eaf7
Viktoriia <viktoriamyhailiak@gmail.com>
2025-08-19 21:01:57 +0300

    add a solution

--- src/components/PeopleFilters.tsx
+++ src/components/PeopleFilters.tsx
@@ -1,18 +1,84 @@
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { SearchLink } from './SearchLink';
+
 export const PeopleFilters = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('queкн') || '';
+  const gender = searchParams.get('sex') || 'all';
+  const centuries = searchParams.getAll('centuries') || [];
+  const [activeLink, setActiveLink] = useState('all');
+
+  function handleQueryChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const params = new URLSearchParams(searchParams);
+
+    params.set('query', e.target.value);
+    setSearchParams(params);
+  }
+
+  function handleLinkSelection(sex: string) {
+    const params = new URLSearchParams(searchParams);
+
+    setActiveLink(sex);
+
+    params.set('sex', sex);
+    setSearchParams(params);
+  }
+
+  function clearCenturies() {
+    const params = new URLSearchParams(searchParams);
+
+    params.delete('centuries');
+    setSearchParams(params);
+  }
+
+  function clearAll() {
+    setSearchParams('');
+  }
+
+  function toggleCenturies(x: number) {
+    const params = new URLSearchParams(searchParams);
+
+    const newCenturies = centuries.includes(x.toString())
+      ? centuries.filter(y => y.toString() !== x.toString())
+      : [...centuries, x.toString()];
+
+    params.delete('centuries');
+    newCenturies.forEach(century =>
+      params.append('centuries', century.toString()),
+    );
+    setSearchParams(params);
+  }
+
   return (
     <nav className="panel">
       <p className="panel-heading">Filters</p>
 
       <p className="panel-tabs" data-cy="SexFilter">
-        <a className="is-active" href="#/people">
+        <SearchLink
+          params={{ sex: null }}
+          className={classNames({ 'is-active': activeLink === 'all' })}
+          onClick={() => handleLinkSelection('all')}
+        >
           All
-        </a>
-        <a className="" href="#/people?sex=m">
+        </SearchLink>
+
+        <SearchLink
+          params={{ sex: 'm' }}
+          className={classNames({ 'is-active': activeLink === 'male' })}
+          onClick={() => handleLinkSelection('male')}
+        >
           Male
-        </a>
-        <a className="" href="#/people?sex=f">
+        </SearchLink>
+
+        <SearchLink
+          params={{ sex: 'f' }}
+          className={classNames({ 'is-active': activeLink === 'female' })}
+          onClick={() => handleLinkSelection('female')}
+        >
           Female
-        </a>
+        </SearchLink>
       </p>
 
       <div className="panel-block">
@@ -22,6 +88,7 @@ export const PeopleFilters = () => {
             type="search"
             className="input"
             placeholder="Search"
+            onChange={e => handleQueryChange(e)}
           />
 
           <span className="icon is-left">
@@ -33,45 +100,60 @@ export const PeopleFilters = () => {
       <div className="panel-block">
         <div className="level is-flex-grow-1 is-mobile" data-cy="CenturyFilter">
           <div className="level-left">
-            <a
+            <SearchLink
+              params={{ centuries: [...centuries, '16'] }}
               data-cy="century"
-              className="button mr-1"
-              href="#/people?centuries=16"
+              className={classNames('button mr-1', {
+                'is-info': centuries.includes('16'),
+              })}
+              onClick={() => toggleCenturies(16)}
             >
               16
-            </a>
+            </SearchLink>
 
-            <a
+            <SearchLink
+              params={{ centuries: [...centuries, '17'] }}
               data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=17"
+              className={classNames('button mr-1', {
+                'is-info': centuries.includes('17'),
+              })}
+              onClick={() => toggleCenturies(17)}
             >
               17
-            </a>
+            </SearchLink>
 
-            <a
+            <SearchLink
+              params={{ centuries: [...centuries, '18'] }}
               data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=18"
+              className={classNames('button mr-1', {
+                'is-info': centuries.includes('18'),
+              })}
+              onClick={() => toggleCenturies(18)}
             >
               18
-            </a>
+            </SearchLink>
 
-            <a
+            <SearchLink
+              params={{ centuries: [...centuries, '19'] }}
               data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=19"
+              className={classNames('button mr-1', {
+                'is-info': centuries.includes('19'),
+              })}
+              onClick={() => toggleCenturies(19)}
             >
               19
-            </a>
+            </SearchLink>
 
-            <a
+            <SearchLink
+              params={{ centuries: [...centuries, '20'] }}
               data-cy="century"
-              className="button mr-1"
-              href="#/people?centuries=20"
+              className={classNames('button mr-1', {
+                'is-info': centuries.includes('20'),
+              })}
+              onClick={() => toggleCenturies(20)}
             >
               20
-            </a>
+            </SearchLink>
           </div>
 
           <div className="level-right ml-4">
@@ -79,6 +161,7 @@ export const PeopleFilters = () => {
               data-cy="centuryALL"
               className="button is-success is-outlined"
               href="#/people"
+              onClick={() => clearCenturies()}
             >
               All
             </a>
@@ -87,7 +170,11 @@ export const PeopleFilters = () => {
       </div>
 
       <div className="panel-block">
-        <a className="button is-link is-outlined is-fullwidth" href="#/people">
+        <a
+          className="button is-link is-outlined is-fullwidth"
+          href="#/people"
+          onClick={() => clearAll()}
+        >
           Reset all filters
         </a>
       </div>
--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,33 +1,68 @@
-import { PeopleFilters } from './PeopleFilters';
+import { useEffect, useState } from 'react';
 import { Loader } from './Loader';
+import { getPeople } from '../api';
+import { Person } from '../types';
 import { PeopleTable } from './PeopleTable';
+import { useParams } from 'react-router-dom';
+import { PeopleFilters } from './PeopleFilters';
 
 export const PeoplePage = () => {
+  const [error, setError] = useState(false);
+  const [people, setPeople] = useState<Person[] | []>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const { slug } = useParams();
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getPeople()
+      .then(result => {
+        setPeople(result);
+      })
+      .catch(() => setError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
-    <>
-      <h1 className="title">People Page</h1>
+    <div className="section">
+      <div className="container">
+        <h1 className="title">People Page</h1>
 
-      <div className="block">
-        <div className="columns is-desktop is-flex-direction-row-reverse">
-          <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
-          </div>
+        <div className="block">
+          <div className="columns is-desktop is-flex-direction-row-reverse">
+            <div className="column is-7-tablet is-narrow-desktop">
+              {!isLoading && people.length > 0 && (
+                <PeopleFilters setPeople={setPeople} />
+              )}
+            </div>
 
-          <div className="column">
-            <div className="box table-container">
-              <Loader />
+            <div className="column">
+              <div className="box table-container">
+                {isLoading ? (
+                  <Loader />
+                ) : (
+                  <PeopleTable
+                    people={people}
+                    selected={slug?.toString() || ''}
+                  />
+                )}
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+                {error && (
+                  <p data-cy="peopleLoadingError" className="has-text-danger">
+                    Something went wrong
+                  </p>
+                )}
 
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
-
-              <p>There are no people matching the current search criteria</p>
-
-              <PeopleTable />
+                {people.length === 0 && !isLoading && (
+                  <p data-cy="noPeopleMessage">
+                    There are no people on the server
+                  </p>
+                )}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
